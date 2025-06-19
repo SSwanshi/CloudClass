1fa7e2db99fa8ecf4bff0b4176bf1e726c9047f7
Sarvjeet Swanshi <adityaspgm@gmail.com>
2025-06-19 21:40:54 +0530

    last left

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -94,7 +94,7 @@ export async function DELETE(
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { courseId: string; chapterId: string } }
+  { params }: { params: Promise<{ courseId: string; chapterId: string }> }
 ) {
   try {
     const { userId } = await auth();
