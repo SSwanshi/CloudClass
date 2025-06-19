2778de8dbda5825800a591bb8f91426a3f05cd54
Sarvjeet Swanshi <adityaspgm@gmail.com>
2025-06-19 20:11:55 +0530

    layout fixed

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -1,32 +1,25 @@
-import { auth } from "@clerk/nextjs/server";;
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
-
 import { getProgress } from "@/actions/get-progress";
 
-
-// import getSafeProfile from "@/actions/get-safe-profile";
 import { CourseSidebar } from "./_components/course-sidebar";
 import { CourseNavbar } from "./_components/course-navbar";
 
 const CourseLayout = async ({
   children,
-  params
+  params,
 }: {
   children: React.ReactNode;
   params: { courseId: string };
 }) => {
   const { userId } = await auth();
   if (!userId) {
-    return redirect("/")
+    return redirect("/");
   }
-//   const safeProfile = await getSafeProfile();
-//   if (!safeProfile) {
-//     return redirect("/");
-//   }
 
-  const {courseId} = await params;
+  const { courseId } = params;
 
   const course = await db.course.findUnique({
     where: {
@@ -41,12 +34,12 @@ const CourseLayout = async ({
           userProgress: {
             where: {
               userId,
-            }
-          }
+            },
+          },
         },
         orderBy: {
-          position: "asc"
-        }
+          position: "asc",
+        },
       },
     },
   });
@@ -58,27 +51,16 @@ const CourseLayout = async ({
   const progressCount: number = await getProgress(userId, course.id);
 
   return (
-
     <div className="h-full">
       <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
-        <CourseNavbar
-          course={course}
-          progressCount={progressCount}
-        //   currentProfile={safeProfile}
-        />
+        <CourseNavbar course={course} progressCount={progressCount} />
       </div>
       <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
-        <CourseSidebar
-          course={course}
-          progressCount={progressCount}
-        />
+        <CourseSidebar course={course} progressCount={progressCount} />
       </div>
-      <main className="md:pl-80 pt-[80px] h-full">
-        {children}
-      </main>
+      <main className="md:pl-80 pt-[80px] h-full">{children}</main>
     </div>
+  );
+};
 
-  )
-}
-
-export default CourseLayout
+export default CourseLayout;
